9a6c9eb5935fafe5660fb0b7a17f0859bf764ee7
Angela <angelacr@ymail.com>
2021-04-18 22:50:31 -0500

    adding values to department and role

---
+++ db/seed.sql
@@ -0,0 +1,19 @@
+USE employee_db;
+
+
+INSERT INTO department (name)
+VALUES
+("Sales"),
+("Marketing"),
+("HR"),
+("Engineering");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+("Sales Rep", 35000, 1),
+("Marketeer", 60000, 2),
+("Manager", 70000, 3),
+("Engineer", 52000, 4);
+
+
